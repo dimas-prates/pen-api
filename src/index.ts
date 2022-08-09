23e5b97d6233806530987832fd078d2630b6a283
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-09 16:38:27 -0300

    :sparkles: feat(index.ts): Basic server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
-
-AppDataSource.initialize().then(async () => {
-
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+import express from "express";
+import { AppDataSource } from "./data-source";
+import routes from './routes'
+const appPort = 3000 || process.env.PORT
+AppDataSource.initialize().then(() => {
+    const app = express();
+    app.use(express.json())
+    // app.get("/", (req, res) => {
+    //     return res.json("App initialized")
+    // })
+    app.use(routes)
+    return app.listen(appPort, () => { console.log(`Application located at "${__dirname}" and listening on port: ${appPort}`) })
+})
