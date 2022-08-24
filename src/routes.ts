004500088d3c1e32f84f28d9108c7b033f7643f1
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-24 08:48:26 -0300

    :test_tube: test(routes.ts): execute some tests for error handling

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { RoomController } from "./controllers/RoomController";
 import { SubjectController } from "./controllers/SubjectController";
+// import { BadRequestError, ErrorApi } from "./helpers/error-api";
 
 const routes = Router();
 routes.post('/subject', new SubjectController().create)
@@ -9,5 +10,8 @@ routes.get('/room', new RoomController().list)
 routes.get('/room/:idRoom', new RoomController().listOne)
 routes.post('/room/:idRoom/create', new RoomController().createVideo)
 routes.post('/room/:idRoom/subject', new RoomController().roomSubject)
-
+// routes.get('/', async (req, res) => {
+//     throw new ErrorApi('Erro forçado', 404)
+//     throw new BadRequestError('not found')
+// })
 export default routes
