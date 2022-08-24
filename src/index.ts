a3b78387eaaa4945b61a03196fd1e23a71bbafa5
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-24 08:50:45 -0300

    :sparkles: feat(index.ts): add error middleware and adjust env port

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import 'express-async-errors'
+import morgan from "morgan";
 import { AppDataSource } from "./data-source";
 import routes from './routes'
-const appPort = 3000 || process.env.PORT
+import { errorHandling } from './middlewares/error'
+
+const appPort = process.env.PORT || 3000
 AppDataSource.initialize().then(() => {
     const app = express();
+    app.use(morgan('dev'))
     app.use(express.json())
     // app.get("/", (req, res) => {
     //     return res.json("App initialized")
     // })
     app.use(routes)
+    app.use(errorHandling)
     return app.listen(appPort, () => { console.log(`Application located at "${__dirname}" and listening on port: ${appPort}`) })
 })
